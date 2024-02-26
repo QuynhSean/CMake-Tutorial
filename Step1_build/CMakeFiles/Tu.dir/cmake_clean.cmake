8780af3371e19d48cc3043dd7f70eb4c2bb98266
quynh <tieuchien123@gmail.com>
2024-02-26 13:33:18 +0700

    done ex1

---
+++ Step1_build/CMakeFiles/Tu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tu.dir/tutorial.cxx.o"
+  "CMakeFiles/Tu.dir/tutorial.cxx.o.d"
+  "Tu"
+  "Tu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
